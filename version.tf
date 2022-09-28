62e0189d5bad799e50d574a948b33a6b6f2db89e
David J. M. Karlsen <david@davidkarlsen.com>
2022-09-28 09:11:25 +0200

    require TF 1.3 where optionals are GA

--- version.tf
+++ version.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = ">=0.13.0"
+  required_version = ">=1.3.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">=2.0.0"
     }
   }
-  experiments = [module_variable_optional_attrs]
 }
